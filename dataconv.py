18e969fd39d683ffdd34742fd1abf93d7b7c85c3
tristan <tristan.stoeber@posteo.net>
2018-10-04 13:21:50 +0200

    adsf

--- dataconv.py
+++ dataconv.py
@@ -6,6 +6,8 @@ import tarfile as tf
 import neo
 import quantities as pq
 import xml.etree.ElementTree as et
+import helpfuncs as hf
+import pdb
 
 
 def regexp(expr, item):
@@ -22,7 +24,9 @@ class ReadSession:
                  beh,
                  session,
                  unit_spiketime,
-                 unit_space):
+                 unit_space,
+                 unit_lfp=pq.V,
+                 load_lfp=False):
 
         meta_path = path + dataset + '/docs/' + dataset.replace('-', '') +\
                     '-metadata-tables/' + dataset.replace('-', '') +\
@@ -70,7 +74,31 @@ class ReadSession:
             'From cell where topdir REGEXP \'' +
             topdir + '\'',
             con_sessions)
-
+        
+        df_epos = pd.read_sql_query(
+            'SELECT ' +
+            'topdir, ' +
+            'animal, ' +
+            'e1, ' +
+            'e2, ' +
+            'e3, ' +
+            'e4, ' +
+            'e5, ' +
+            'e6, ' +
+            'e7, ' +
+            'e8, ' +
+            'e9, ' +
+            'e10, ' +
+            'e11, ' +
+            'e12, ' +
+            'e13, ' +
+            'e14, ' +
+            'e15, ' +
+            'e16 ' +
+            'From epos where topdir REGEXP \'' +
+            topdir + '\'',
+            con_sessions)
+        
         electrode_ids = np.unique(df_cells['ele'])
         path_to_session = path + dataset + '/' + \
             topdir + '/' +\
@@ -133,10 +161,39 @@ class ReadSession:
                 times_i = times_i[pos_cluster_not_0_or_1]
                 clusters[ele_i] = clusters_i
                 times[ele_i] = times_i
+
+            if load_lfp:
+                xml = hf.etree_to_dict(e)['parameters']
+                dtype_str = xml['acquisitionSystem']['nBits']
+                if dtype_str == '16':
+                    lfp_dtype = np.int16
+                lfp_n_channels = int(xml['acquisitionSystem']['nChannels'])
+                lfp_samplingrate = int(xml['fieldPotentials']['lfpSamplingRate']) * pq.Hz
+                lfp_amplification = 1./np.float(xml['acquisitionSystem']['amplification']) 
+                eeg_f = tf_session.extractfile(
+                    topdir + '/' +
+                    subdir + '/' +
+                    subdir + '.eeg')
+                eeg_f_content = eeg_f.read()
+                lfp_raw = np.fromstring(eeg_f_content, dtype=np.int16)
+                #                lfp_raw = np.fromfile(eeg_f, dtype=np.int16)
+                lfp = np.zeros((int(len(lfp_raw)/lfp_n_channels), lfp_n_channels))
+                for i in range(lfp_n_channels):
+                    lfp[:, i] = lfp_raw[i::lfp_n_channels]
+                lfp *= lfp_amplification
+                lfp *= unit_lfp
+                lfp_n_electrodes = len(xml['anatomicalDescription']['channelGroups']['group'])-1
+                lfp_channel_groups = []
+                for i in range(lfp_n_electrodes):
+                    channel_group_i = []
+                    for row in xml['anatomicalDescription']['channelGroups']['group'][i]['channel']:
+                        channel_group_i.append(int(row['#text']))
+                    lfp_channel_groups.append(channel_group_i)
+                        # each entry in channel groups, corresponds to an electrode number
                 
+                    
         positions = positions * unit_space
         # create neo structure to hold represent data.
-        print('Convert to neo')
         blk = neo.Block(animal_id=animal_id,
                         dataset=dataset,
                         path=path,
@@ -150,7 +207,7 @@ class ReadSession:
         chx_units = neo.ChannelIndex(index=0,
                                      name='units')
         blk.channel_indexes.append(chx_units)
-        chx_pos = neo.ChannelIndex(index=0,
+        chx_pos = neo.ChannelIndex(index=1,
                                    name='position')
         blk.channel_indexes.append(chx_pos)
         sampling_rate_pos = 39.0625 * pq.Hz
@@ -174,7 +231,7 @@ class ReadSession:
                                       name='led1_y',
                                       led_pos='posterior')
         chx_pos.analogsignals.append(pos_led1_y)
-
+        
         for _, row_i in df_cells.iterrows():
             # create unit
             unit_i = neo.Unit(name=row_i['id'],
@@ -202,6 +259,26 @@ class ReadSession:
                 unit_i.spiketrains.append(train_i)
             chx_units.units.append(unit_i)
 
+        if load_lfp:
+            print('Get LFP')
+            for i in range(lfp_n_electrodes):
+                name = 'e'+str(i+1)
+                # get electrode position
+                epos_i = df_epos[name].loc[0]
+                last_idx = int(blk.channel_indexes[-1].index)
+                chx_lfp_i = neo.ChannelIndex(index=last_idx+1+i,
+                                             name='lfp_' + name,
+                                             channel_indexes=lfp_channel_groups[i],
+                                             coordinates=epos_i,
+                                             electrode=i+1)
+                analog_i = []
+                for j in lfp_channel_groups[i]:
+                    analog_i.append(lfp[:, j])
+                analog_i = np.vstack(analog_i).T
+                sig_i = neo.AnalogSignal(analog_i, units=unit_lfp, sampling_rate=lfp_samplingrate)
+                chx_lfp_i.analogsignals.append(sig_i)
+                blk.channel_indexes.append(chx_lfp_i)
+            
         self.df_session = df_session
         self.df_cells = df_cells
         self.dataset = dataset
